381e9200935990c7b854f169c67163e4e7fc3b98
Robel Haftom <robelhaftom80@gmail.com>
2025-03-20 21:51:19 +0300

    setting up front end

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+import { Router, Routes, Route } from "react-router-dom";
 import SignUp from "./todofrontend/pages/SignUp";
-import SignIn from "./todofrontend/pages/SignIn";
-import ProfilePage from "./todofrontend/pages/ProfilePage";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/profile/*" element={<ProfilePage />} />
-        </Routes>
-      </Router>
-    </>
-  );
+  return <SignUp />;
 }
 
 export default App;
---
+++ Frontend/src/todofrontend/assets/assets.js
@@ -0,0 +1,49 @@
+import alert_pink from "./alert_pink.png";
+import alert_white from "./alert_white.png";
+import bg from "./bg.jpeg";
+import clipboards_pink from "./clipboards_pink.png";
+import clipboards_white from "./clipboards_white.png";
+import dashboard_pink from "./dashboard_pink.png";
+import dashboard_white from "./dashboard_white.png";
+import edit from "./edit.png";
+import email from "./email.png";
+import list_pink from "./list_pink.png";
+import list_white from "./list_white.png";
+import logout_pink from "./logout_pink.png";
+import logout_white from "./logout_white.png";
+import molle from "./molle.jpg";
+import padlock_lock from "./padlock_lock.png";
+import padlock_open from "./padlock_open.png";
+import question_pink from "./question_pink.png";
+import question_white from "./question_white.png";
+import setting_pink from "./setting_pink.png";
+import setting_white from "./setting_white.png";
+import user from "./user.png";
+import sign_in_background from "./sign-in-background.jpg";
+import sign_up_background from "./sign-up-background.jpg";
+
+export {
+  alert_pink,
+  alert_white,
+  bg,
+  clipboards_pink,
+  clipboards_white,
+  dashboard_pink,
+  dashboard_white,
+  edit,
+  email,
+  list_pink,
+  list_white,
+  logout_pink,
+  logout_white,
+  molle,
+  padlock_lock,
+  padlock_open,
+  question_pink,
+  question_white,
+  setting_pink,
+  setting_white,
+  user,
+  sign_in_background,
+  sign_up_background,
+};
